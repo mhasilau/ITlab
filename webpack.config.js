165df13123eca72459a609dd336034503682c759
”Mikhail <mikhail.hasilau@gmail.com>
2021-07-15 15:59:28 +0300

    name without picture

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
-        test: /\.(png|jpg|svg,gif|jpeg)$/,
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
         use: 'file-loader'
       }
     ]
